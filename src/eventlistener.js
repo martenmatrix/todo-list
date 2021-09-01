bb73a93f66af959ce8c7ffe0c05d7e3a9a41d2a1
Marten <derapfelesser@aol.com>
2021-09-01 13:15:00 +0200

    Add add buttons for todo and lists

---
+++ src/eventlistener.js
@@ -0,0 +1,17 @@
+/* on start check for cookies and apply them
+on every new button press set new cookies
+
+if user deleted a list must remove list from list array, clean todos dom and regenerate list dom
+if user deletes list must remove todo from list items array and regenrate todo page, plus insert undo module like below, maybe savce the last array in a misc function
+
+if user adds todo add to array and update dom
+if user deletes todo remove from array update dom and save in var as last todo + display last delted
+if user undos remove fromm array add back to list and remove the displayed undo
+
+
+*/
+const listenFor = (function() {
+    function undoButton() {
+
+    };
+})();
